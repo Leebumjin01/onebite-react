ff02e95b382ad820cb374eba7e9e1ec22bb76934
lbj0797 <lbj0797@dataeum.kr>
2024-10-31 14:37:15 +0900

    React App 생성

--- section03/src/index.js
+++ section03/src/index.js
@@ -5,6 +5,11 @@ import multyply from "./math.js";
 // math.js 동일한 경로에서 불러올 때, 아래처럼 import도 가능
 import multyply, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multyply(2, 3));
+// random color 라이브러리 호출
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multyply(2, 3));
