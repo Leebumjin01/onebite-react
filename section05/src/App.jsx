7ee02bdad6563ae65eed621b6eca2a8ee126e657
lbj0797 <lbj0797@dataeum.kr>
2024-10-31 17:21:25 +0900

    Count 컴포넌트, Bulb 컴포넌트 분리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
 import { useState } from "react";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Count";
+
 /* 
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
@@ -7,8 +10,12 @@ import Footer from "./components/Footer.jsx";
 import Button from "./components/Button.jsx";
  */
 
-// root 컴포넌트
+// 리액트 컴포넌트는 3가지 상황에서 리렌더링 됨
+// 1. 자신이 관리하는 state 값이 변경 되었을 때
+// 2. 자신이 관리하는 props 값이 변경 되었을 때
+// 3. 부모 컴포넌트가 리렌더링 되었을 때 자식 컴포넌트도 리렌더링 됨
 
+// App() 은 root 컴포넌트
 function App() {
   /* 
   const buttonProps = {
@@ -23,31 +30,12 @@ function App() {
 
   // state변수는 useState에서 생성된 값이 저장
   // setState는 state의 값을 변경시키는 함수가 저장
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "OFF" ? "ON" : "OFF");
-          }}
-        >
-          {light === "OFF" ? "켜기" : "끄기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Bulb></Bulb>
+      {/* <h1>{light}</h1> */}
+
       {/* 자식 컴포넌트 */}
       {/*<Button text={"메일"} color="red"></Button>*/}
       {/* Spread 사용 */}
@@ -58,6 +46,8 @@ function App() {
         <Header />
       </Button>
        */}
+
+      <Counter></Counter>
     </>
   );
 }
