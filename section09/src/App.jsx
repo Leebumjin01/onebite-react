11deb53537c2cbcea7347d2bffbd7edceb2f01f1
lbj0797 <lbj0797@dataeum.kr>
2024-11-05 11:00:32 +0900

    useReducer 실습 전 투두리스트 프로젝트 복사

---
+++ section09/src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+import { useState, useRef } from "react";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo) =>
+        // true면 isDone 을 반전(토글)
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const onDelete = (targetId) => {
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    setTodos(todos.filter((todo) => todo.id !== targetId));
+  };
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Editor onCreate={onCreate}></Editor>
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}></List>
+    </div>
+  );
+}
+
+export default App;
