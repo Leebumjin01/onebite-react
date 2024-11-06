97a25b6c1ab491ade5434200fa94790cc699acce
lbj0797 <lbj0797@dataeum.kr>
2024-11-06 18:02:04 +0900

    Home 페이지 구현 - UI

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={"2024년 11월"}
+        leftChild={<Button text={"<"}></Button>}
+        rightChild={<Button text={">"}></Button>}
+      ></Header>
+      <DiaryList></DiaryList>
+    </div>
+  );
 };
 
 export default Home;
