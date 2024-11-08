a9adfc2373c8658312ce00b49927d0f9372f6ca8
lbj0797 <lbj0797@dataeum.kr>
2024-11-08 10:13:36 +0900

    New 페이지 기능 구현

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,8 +1,53 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
+  // useParams: URL의 파라미터를 제공
   const params = useParams();
-  return <div>{params.id}번 수정 페이지 입니다</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("삭제하시겠습니까?")) {
+      onDelete(params.id);
+      // 브라우저의 내장기능을 사용하는 함수, 확인, 취소가 있는 팝업창을 띄워줌
+      // t, f 반환
+      nav("/", { replace: true });
+    }
+  };
+
+  const getCurrentDiaryItem = () => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", { replace: true });
+    }
+    return currentDiaryItem;
+  };
+
+  const currentDiaryItem = getCurrentDiaryItem();
+  console.log(currentDiaryItem);
+
+  return (
+    <div>
+      <Header
+        title={"수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< Back"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제"} type={"NEGATIVE"} />
+        }
+      ></Header>
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,15 +1,26 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
   return (
     <div>
       <Header
-        leftChild={<Button text={"< BACK"}></Button>}
         title={"새 일기 쓰기"}
+        // navigate 의 인수에 -1을 전달하면 뒤로가기
+        leftChild={<Button onClick={() => nav(-1)} text={"< BACK"}></Button>}
       />
-      <Editor></Editor>
+      <Editor onSubmit={onSubmit}></Editor>
     </div>
   );
 };
